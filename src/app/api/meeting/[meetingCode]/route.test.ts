8b160610e89dc71874896bade4a4e6625ec49ca9
Frederik Claus <f.v.claus@googlemail.com>
2024-12-21 12:06:31 +0100

    More tests

--- src/app/api/meeting/[meetingCode]/route.test.ts
+++ src/app/api/meeting/[meetingCode]/route.test.ts
@@ -1,34 +1,51 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi, beforeEach, Mock } from 'vitest';
 import * as handler from './route';
+import {findMeeting} from './findMeeting';
+import { getSessionKey } from '@/session-store';
 
-describe('Meeting Route', () => {
 
+describe('/api/meeting/[meetingCode]', () => {
 
-    it('should return meeting details for a valid meeting code', async () => {
-        vi.mock('./findMeeting', () => ({
-            findMeeting: async function() {
-                return {
-                    scheduledEndTime: 'foo',
-                    code: "foo",
-                    name: "foo",
-                    uri: "foo",
-                    userId: "foo"
-                }
+    beforeEach(() => {
+        vi.mock('./findMeeting');
+        vi.mock('./../../../../session-store');
+        vi.resetAllMocks();
+        const findMeetingMock: typeof findMeeting = async (params) => {
+            const meetingCode = (await params).meetingCode;
+            return {
+                code: meetingCode,
+                uri: meetingCode,
+                userId: `${meetingCode}_userId`,
+                name: 'name',
+                scheduledEndTime: 'scheduledEndTime'
             }
-        }));
-        vi.mock('./../../../../session-store', () => ({
-            getSessionKey: async function() {
-                return 'foo'
-            }
-        }));
-        const response = await handler.GET(null as any, {params: Promise.resolve({meetingCode: "validMeeting"})});
-        const data = await response.json();
-        expect(response.status).toBe(200);
-        expect(data).toHaveProperty('uri', 'foo');
-    });
+        }
+        vi.mocked(findMeeting).mockImplementation(findMeetingMock)
+    })
 
-    it.skip('should return 404 for an invalid meeting code', async () => {
-        const response = await fetch('/api/meeting/invalidMeetingCode');
-        expect(response.status).toBe(404);
-    });
+    describe('GET', () => {
+        it('should return meeting details for a valid meeting code', async () => {
+            vi.mocked(getSessionKey).mockResolvedValue('validMeeting_userId');
+            const response = await handler.GET(null as any, {params: Promise.resolve({meetingCode: "validMeeting"})});
+            expect(response.status).toBe(200);
+            const data = await response.json();
+            expect(data).toEqual({uri: 'validMeeting', scheduledEndTime: 'scheduledEndTime'});
+        });
+    
+        it('should return 403 for an invalid meeting code', async () => {
+            vi.mocked(findMeeting).mockResolvedValue(undefined);
+            const response = await handler.GET(null as any, {params: Promise.resolve({meetingCode: "invalidMeeting"})});
+            expect(vi.mocked(getSessionKey)).toHaveBeenCalledTimes(0);
+            expect(response.status).toBe(403);
+        });
+    
+        it('should return 403 for a meeting code that belongs to other person', async () => {
+            vi.mocked(getSessionKey).mockResolvedValue('otherUser');
+            const response = await handler.GET(null as any, {params: Promise.resolve({meetingCode: "validMeeting"})});
+            expect(vi.mocked(getSessionKey)).toHaveBeenCalledOnce();
+            expect(response.status).toBe(403);
+        });
+    })
+
+
 });
