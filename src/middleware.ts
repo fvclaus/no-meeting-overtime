437e09a22f04134f866841e2f6f093dd786e7894
Frederik Claus <f.v.claus@googlemail.com>
2025-01-11 14:20:48 +0100

    Adds middleware to redirect to non www sites

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const url = request.nextUrl.clone();
-  console.log(`Looking at ${url.hostname}`);
-  if (url.hostname.startsWith("www.")) {
-    url.hostname = url.hostname.replace("www.", "");
-    return NextResponse.redirect(url);
+  const url = request.headers.get("X-Forwarded-Host");
+  console.log(`Looking at ${url}`);
+  if (url !== null && url.startsWith("www.")) {
+    return NextResponse.redirect(url.replace("www.", ""));
   }
   return NextResponse.next();
 }
