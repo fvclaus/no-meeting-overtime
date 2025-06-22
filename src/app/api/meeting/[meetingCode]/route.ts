36baa5f4f7e06aa3f4363337ea607067a49be628
Frederik Claus <f.v.claus@googlemail.com>
2025-06-22 12:30:04 +0200

    Upgrade to Next 15 (#3)

--- src/app/api/meeting/[meetingCode]/route.ts
+++ src/app/api/meeting/[meetingCode]/route.ts
@@ -18,17 +18,15 @@ export type RouteParams = Promise<{
   meetingCode: string;
 }>;
 
-export async function GET(
-  req: NextRequest,
-  { params }: { params: RouteParams },
-) {
+export async function GET(req: NextRequest, props: { params: RouteParams }) {
+  const params = await props.params;
   const sessionData = await getSession();
 
   if (!isAuthorizedSession(sessionData)) {
     return new NextResponse("Unauthenticated", { status: 403 });
   }
 
-  const meeting = await findMeeting(params);
+  const meeting = await findMeeting(props.params);
   if (meeting === undefined) {
     return new NextResponse("Unauthenticated", { status: 403 });
   }
@@ -47,10 +45,8 @@ export async function GET(
   });
 }
 
-export async function DELETE(
-  req: NextRequest,
-  { params }: { params: RouteParams },
-) {
+export async function DELETE(req: NextRequest, props: { params: RouteParams }) {
+  const params = await props.params;
   const { meetingCode } = await params;
   const taskName = req.headers.get("X-CLOUDTASKS-TASKNAME");
   if (taskName === null) {
@@ -110,7 +106,7 @@ export async function DELETE(
     refresh_token: user.refresh_token,
   } as Partial<Credentials>);
 
-  const meeting = await findMeeting(params);
+  const meeting = await findMeeting(props.params);
   if (meeting === undefined) {
     logger.error(`[${taskName}]: Did not find meeting`, {
       taskName,
